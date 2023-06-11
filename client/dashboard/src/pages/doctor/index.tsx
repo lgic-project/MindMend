4654fb0418ba4e456a389d995d3408df41c818aa
sapana <sapna.baniya@wowfinstacknepal.com>
2023-06-11 16:44:20 +0545

    integrated mood with api

--- client/dashboard/src/pages/doctor/index.tsx
+++ client/dashboard/src/pages/doctor/index.tsx
@@ -2,10 +2,41 @@ import Head from 'next/head';
 import { Container } from '@mui/material';
 
 import Table from '../../views/dashboard/Table'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { DOCTOR_ROUTE, MOODCATEGORY_ROUTE } from 'src/configs/appRoutes'
 
 function Doctor() {
-  const column = ['Name','Description','Phone','Working hour','working day','Experience','last_Created At','last_creaed By','Image', 'last_Updated At'];
- 
+  // const column = ['Name','Description','Phone','Working hour','working day','Experience','last_Created At','last_creaed By','Image', 'last_Updated At'];
+
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+
+    const GetDoctorList = async ()=>{
+       await axios.get("http://localhost:9091/api/doctor").then((res)=>{
+        // setLoading(true);
+        setData(res.data);
+        setLoading(false);
+        console.log(res)
+
+        const keys = Object.keys(res.data[0]);
+        setColumns(keys);
+
+
+      });
+
+
+
+    }
+    GetDoctorList();
+
+
+
+  },[])
+
   return (
     <>
       <Head>
@@ -14,7 +45,7 @@ function Doctor() {
 
       <Container maxWidth="lg">
 
-          <Table title ="Doctor" columnList={column}></Table>
+          <Table title ="Doctor" columnList={columns} data={data} loading={loading}></Table>
 
       </Container>
 
--- client/dashboard/src/pages/site-config/index.tsx
+++ client/dashboard/src/pages/site-config/index.tsx
@@ -2,9 +2,39 @@ import Head from 'next/head';
 import { Container } from '@mui/material';
 
 import Table from '../../views/dashboard/Table'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { MOODCATEGORY_ROUTE, MOOD_ROUTE } from 'src/configs/appRoutes'
 
 function SiteConfig() {
-  const column = ['Name','Logo','Created At', 'Updated At'];
+  // const column = ['Name','Logo','Created At', 'Updated At'];
+
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+
+    const GetMoodCategoryList = async ()=>{
+       await axios.get(MOOD_ROUTE).then((res)=>{
+        // setLoading(true);
+        setData(res.data);
+        setLoading(false);
+
+        const keys = Object.keys(res.data[0]);
+        setColumns(keys);
+
+
+      });
+
+
+
+    }
+    GetMoodCategoryList();
+
+
+
+  },[])
 
   return (
     <>
@@ -14,7 +44,7 @@ function SiteConfig() {
 
       <Container maxWidth="lg">
 
-          <Table title ="SiteConfig" columnList={column}></Table>
+          <Table title ="SiteConfig" columnList={columns} data={data} loading={loading}></Table>
 
       </Container>
 
