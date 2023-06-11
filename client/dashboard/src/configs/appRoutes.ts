c5d5da49d198c458f02a0fe202c677f4d7af6b99
mahimalgic <mahimasharma7561@gmail.com>
2023-06-11 16:57:05 +0545

    commit

--- client/dashboard/src/configs/appRoutes.ts
+++ client/dashboard/src/configs/appRoutes.ts
@@ -4,5 +4,5 @@ export const SITECONFIG_ROUTE = `${API_BASE_URL}/siteconfig`;
 export const PROFILE_ROUTE = `${API_BASE_URL}/profile`;
 export const MOODCATEGORY_ROUTE = `${API_BASE_URL}/mood/cat`;
 export const MOOD_ROUTE = `${API_BASE_URL}/mood`;
-export const DOCTOR_ROUTE = `${API_BASE_URL}/doctor`;
+export const DOCTOR_ROUTE = `${API_BASE_URL}/doctors`;
 export const DOCTORCATEGORY_ROUTE = `${API_BASE_URL}/doctor/category`;
