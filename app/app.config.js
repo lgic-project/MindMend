3079bf7f39025c44df2757140559c2436e8b0f72
nabraj28 <nabrajpoudel.28@gmail.com>
2023-09-13 12:31:34 +0545

    Styles updated

--- app/app.config.js
+++ app/app.config.js
@@ -20,14 +20,5 @@ export default {
         projectId: "45684598-8866-4002-9905-6424d49caeb1",
       },
     },
-    extras: {
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      measurementId: process.env.MEASUREMENT_ID,
-    },
   },
 }
--- app/components/Home/Profile.js
+++ app/components/Home/Profile.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native"
+import { View, Text, Image, TouchableOpacity } from "react-native"
 import React, { useState, useEffect } from "react"
 import styles from "../../style/homestyles"
 import { useRouter } from "expo-router"
@@ -6,17 +6,13 @@ import axios from "axios"
 import { Buffer } from "buffer"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { PROFILE } from "../../utils/appRoutes"
-import Discover from "../../app/dashboard/Discover"
 
 export default Profile = () => {
 
   const router = useRouter()
-  const [profileData, setProfileData] = useState([])
   const currentDate = new Date()
   const myDate = currentDate.getHours().toString().padStart(2, "0")
   const [image, setImage] = useState("")
-  const [decodeImage, setDecodeImage] = useState("")
-
   const [name, setName] = useState("")
   const [address, setAddress] = useState("")
 
@@ -24,7 +20,6 @@ export default Profile = () => {
   const [error, setError] = useState([])
 
   const handleprofile = async () => {
-    // const router = useRouter();
     await AsyncStorage.setItem("profileName", JSON.stringify(name))
     await AsyncStorage.setItem("profileAddress", JSON.stringify(address))
 
@@ -71,7 +66,6 @@ export default Profile = () => {
       return (
         <Image
           source={{ uri: decodedString }}
-          // source={require("../../assets/Images/myprofile.png")}
           resizeMode="contain"
           style={{ width: 45, height: 45, borderRadius: 50 }
           }
