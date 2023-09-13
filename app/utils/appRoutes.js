9f9d9f1d22ec6d7db26a75b712f676a709577936
nabraj28 <nabrajpoudel.28@gmail.com>
2023-09-13 12:36:21 +0545

    Styles updated

---
+++ app/utils/appRoutes.js
@@ -0,0 +1,29 @@
+export const API_BASE_URL = "http://192.168.1.110:9091/api"
+
+export const DOCTOR = `${API_BASE_URL}/doctor/active`
+export const DOCTOR_BY_ID = `${API_BASE_URL}/doctor`
+
+export const DOCTOR_RATING = `${API_BASE_URL}/doctor/active/rating`
+
+export const MOOD_CATEGORY = `${API_BASE_URL}/mood/cat/active`
+export const MOOD_TRACK = `${API_BASE_URL}/mood`
+
+export const EXERCISE = `${API_BASE_URL}/exercise/active`
+export const EXERCISE_BY_ID = `${API_BASE_URL}/exercise`
+
+export const LOGIN = `${API_BASE_URL}/auth/login`
+export const REGISTER = `${API_BASE_URL}/auth/signup`
+
+export const PROFILE = `${API_BASE_URL}/profile/`
+
+export const COUNTRY = `${API_BASE_URL}/profile/country`
+export const STATE = `${API_BASE_URL}/profile/state/`
+export const CITY = `${API_BASE_URL}/profile/city/`
+export const FRIEND = `${API_BASE_URL}/friend/`
+
+
+
+
+
+
+export const SHARE_PROBLEM = "http://192.168.1.110:9091/chat"
